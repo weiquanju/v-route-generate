97e85f42a5881a8cbbb2a816662dc9799e0f03af
weiquanju <anbine@qq.com>
2022-05-12 14:40:00 +0800

    chore: notes

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@ const isVueFile = (name: string) => /\.vue/.test(name);
 
 export function getRoutes(meta: MetaInfo, options: { pathRoot: string }) {
   /**
-   * 正则匹配src+pathRoot路径部分
-   * 例如：
+   * Regular matching `src` + `pathroot` path part
+   * Example:
    *   new RegExp(`[./]*views/`)
-   *   将去掉 '../views/HomeView.vue'  中 '../views/' 这一部分
+   *   The '../views/' part of '../views/HomeView.vue' will be removed
    */
   const reg = new RegExp(`[./]*${options.pathRoot}`);
 
   /**
-   * 此处是将扁平的路径信息，重新生成树型结构数据
+   * Here is to regenerate the tree structure data from the flat path information
    * ['a/b','a/c/d'] => {a:{b:'b',c:{d：'d'}}}
    */
   const path = Object.keys(meta).reduce((all, c) => {
@@ -23,66 +23,34 @@ export function getRoutes(meta: MetaInfo, options: { pathRoot: string }) {
 
     let tmp = all;
     while (paths.length) {
-      // 路径名、文件名
+      // Path name, file name
       const name: string = paths.shift() || "";
-      // 不是vue文件名，就是路径
-      if (!isVueFile(name)) {
+      
+      if (!isVueFile(name)) {// Path name
         if (!tmp[name]) tmp[name] = {};
         tmp = tmp[name] as Path;
-      } else {
-        const file = name.replace(".vue", ""); // HomeView.vue 文件名转为key则为 HomeView
+      } else {// A Vue file name
+        const file = name.replace(".vue", ""); // If the Vue file name `HomeView.vue` is changed to key, it is `HomeView`
         tmp[file] = c;
       }
     }
     return all;
   }, {} as Path);
-  console.log(path);
+  // console.log(path);
 
   return toArr(path, meta, 0);
 }
 
 const getPath = (name: string, deep: number) =>
   deep > 0
-    ? /* 如果 不是 第一层，不需要 加斜杠*/ name === "HomeView"
+    ? /* If it's not the first layer, you don't need to add a slash. */ name === "HomeView"
       ? ""
       : `${name}`
-    : /* 如果 是   第一层，需要  加斜杠*/ name === "HomeView"
+    : /* If it is the first layer, you need to add a slash. */ name === "HomeView"
     ? "/"
     : `/${name}`;
-/**
- * 嵌套路由示例
- * [
-    {
-      path: "/AboutView", // 第一级 需要 以斜杠/开始
-      component: () => import("../views/AboutView.vue"),
-    },
-    {
-      path: "/",
-      component: () => import("../views/HomeView.vue"),
-    },
-    {
-      path: "/Hello",
-      component: <RouterView />,
-      children: [
-        {
-          path: "HiView", // 第二级及一下 不需要 以斜杠/开始
-          component: () => import("../views/Hello/HiView.vue"),
-        },
-        {
-          path: "ChildA",
-          component: <RouterView />,
-          children: [
-            {
-              path: "",
-              component: () => import("../views/Hello/ChildA/HomeView.vue"),
-            },
-          ],
-        },
-      ],
-    },
-  ]
- */
 
+// Convert the object tree data structure to Vue router array tree structure.
 function toArr(data: Path, meta: MetaInfo, deep: number): Route[] {
   return Object.entries(data).map<Route>(([name, pathInfo]) => {
     if (typeof pathInfo === "string") {
@@ -90,7 +58,7 @@ function toArr(data: Path, meta: MetaInfo, deep: number): Route[] {
     } else {
       return {
         path: getPath(name, deep),
-        component: pathInfo.HomeView ? meta[pathInfo.HomeView] : h(RouterView),
+        component: pathInfo.HomeView ? meta[pathInfo.HomeView] /*If there is a `HomeView.vue` page under the directory*/ : h(RouterView),
         children: toArr(pathInfo, meta, deep + 1),
       };
     }
